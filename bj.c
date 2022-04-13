09e8972c3a747aa9ee7d25576ef2e974c1e511e7
sookyeongyeom <sookyeong.dev@gmail.com>
2022-04-14 06:18:08 +0900

    :wrench: update plan

--- bj.c
+++ bj.c
@@ -2,6 +2,12 @@
 
 int main()
 {
-    char *str = "Hello";
-    printf("%d %d", sizeof(str), sizeof(char));
+    int n1, n2;
+    scanf("%d", &n1);
+    scanf("%d", &n2);
+    printf("%d\n", (n2 % 10) * n1);
+    printf("%d\n", (n2 % 100 / 10) * n1);
+    printf("%d\n", (n2 / 100) * n1);
+    printf("%d", n1 * n2);
+    return 0;
 }
